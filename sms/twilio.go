d926f18a58e6be433f7baaaaa399277f312458be
Henry <henry.rowett@gmail.com>
2022-12-23 18:36:31 +0000

    send message for each number

--- sms/twilio.go
+++ sms/twilio.go
@@ -6,29 +6,35 @@ import (
 	"github.com/twilio/twilio-go"
 	twil "github.com/twilio/twilio-go/rest/api/v2010"
 	"os"
+	"strings"
 )
 
 func Send(body string) error {
 	// read token from env
 	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
 	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	numbers := os.Getenv("NUMBERS")
+
+	numbersArray := strings.Split(numbers, ",")
 
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
 	})
 
-	params := &twil.CreateMessageParams{}
-	params.SetTo("+447805444181")
-	params.SetFrom("+1 445 545 2603")
-	params.SetBody(body)
+	for _, n := range numbersArray {
+		params := &twil.CreateMessageParams{}
+		params.SetTo(n)
+		params.SetFrom("+1 445 545 2603")
+		params.SetBody(body)
 
-	resp, err := client.Api.CreateMessage(params)
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		response, _ := json.Marshal(*resp)
-		fmt.Println("Response: " + string(response))
+		resp, err := client.Api.CreateMessage(params)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			response, _ := json.Marshal(*resp)
+			fmt.Println("Response: " + string(response))
+		}
 	}
 
 	return nil

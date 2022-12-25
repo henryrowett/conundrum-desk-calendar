881616af2dd19c46e2654dc998ff32fd99282a8e
Henry <henry.rowett@gmail.com>
2022-12-25 19:58:22 +0000

    change template

--- rounds/templates.go
+++ rounds/templates.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-const titleTemplate = "  \n🕛 HENRY'S COUNTDOWN CALENDAR (%s) 🕡\n\n"
+const titleTemplate = "  \n🕛 HENRY'S COUNTDOWN CALENDAR %s 🕡\n\n"
 const todayTemplate = "Today's puzzles:\n%s puzzle: %s\n%s: %s\n\n"
 const yesterdayTemplate = "Yesterday's answers:\n%s puzzle: %s\n%s: %s"
 
 func Title() string {
-	return fmt.Sprintf(titleTemplate, time.Now().Format("02-01-2006"))
+	return fmt.Sprintf(titleTemplate, time.Now().Format("02/01/2006"))
 }
 
 func TodayString(puzzle Result, conundrum Result) string {
